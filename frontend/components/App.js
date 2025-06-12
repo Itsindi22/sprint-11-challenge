a212e646a8ed07b99a2c3619c9c262ca965d652f
Inderpreet <inderpreet2004123@gmail.com>
2025-06-11 22:07:49 -0400

    2 fail test

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -36,7 +36,7 @@ export default function App() {
   }
 
   const login = async ({ username, password }) => {
-    setSpinnerOn(false)
+    setSpinnerOn()
     setMessage('')
     try {
       const response = await axios.post(
